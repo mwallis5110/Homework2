58843fc5c0b1730ad3e22f500dd68caf5e2d3e7d
Mason <mwallis5110@gmail.com>
2021-09-04 08:46:38 -0500

    Footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,6 @@
 import React from "react";
 // import Button from "react-bootstrap/Button";
 
-
-const github = "https://github.com/mwallis5110";
-const resume = "assets/images/Mason_Wallis_Resume.pdf";
-const emailAddress = "mailto: mwallis5110@gmail.com";
-
 export default function Footer() {
   return (
     <container>
@@ -18,13 +13,17 @@ export default function Footer() {
             </h4>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-            <h4>({github})Github</h4>
+            <h4>
+              <a href="https://github.com/mwallis5110">Github</a>
+            </h4>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-            <h4>({resume})My Resumé</h4>
+            <h4>
+              <a href="assets/images/Mason_Wallis_Resume.pdf" download>My Resumé</a>
+            </h4>
           </div>
           <div className="col-lg-3 col-md-6 col-sm-12">
-            <h4>({emailAddress})Email Me</h4>
+            <h4><a href="mailto: mwallis5110@gmail.com">Email Me</a></h4>
           </div>
         </div>
       </footer>
