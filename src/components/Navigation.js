92430ce1cecda69ec08c184008ed3fd494f43f39
Mason <mwallis5110@gmail.com>
2021-09-27 18:39:00 -0500

    modified:   package-lock.json modified:   package.json modified:   src/components/Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
-import About from "../pages/About/about";
-import Projects from "../pages/Projects/projects";
+import About from "../pages/About/about.js";
+import Projects from "../pages/Projects/projects.js";
 
 export default function Navigation(props) {
   function displayCurrentPage() {
