cc90a4e038ad3e0b5491a30106f91281602b7ac1
Mason <mwallis5110@gmail.com>
2022-03-08 08:49:55 -0600

    Built out projects array

---
+++ src/projectData/projectData.js
@@ -0,0 +1,79 @@
+import Chef from "../../../src/images/sous-chef.PNG";
+import Fitness from "../../../src/images/fitness_tracker.PNG";
+import Employee from "../../../src/images/employee_tracker.gif";
+import Note from "../../../src/images/note_taker.gif";
+import Nphase from "../../../src/images/nphase3.png";
+import Pet from "../../../src/images/pet-seeker.PNG";
+import PortfolioGen from "../../../src/images/portfolioGenerator.PNG";
+
+export const JSArray = [
+  {
+    cardImg: Pet,
+    cardTitle: "Pet Seeker",
+    cardDescription:
+      'Pet Seeker is a user-friendly website that can help you find your new four-legged friend! Imports data from <a href="https://www.petfinder.com/"> PetFinder</a> and <a href="https://thedogapi.com/"> The Dog API</a>',
+    link: "https://brandonnorsworthy.github.io/pet-seeker/",
+    repo: "https://github.com/mwallis5110/pet-seeker",
+  },
+  {
+    cardImg: Note,
+    cardTitle: "ExpressJS Note Taker",
+    cardDescription: "A note taking app built using ExpressJS.",
+    github: "https://github.com/mwallis5110/Note_Taker_HW11",
+  },
+];
+
+export const ReactArray = [
+  {
+    cardImg: Nphase,
+    cardTitle: "NPhase Records Portfolio",
+    cardDescription:
+      "A ReactJS-based portfolio website created for NPhase Records. **Under Construction**",
+    link: "https://music-portfolio-20.herokuapp.com/",
+    repo: "https://github.com/mwallis5110/Music_Portfolio",
+  },
+  {
+    cardImg: "",
+    cardTitle: "My Portfolio",
+    cardDescription:
+      "My personal portfolio, the website you're on right now. Built with ReactJS and NodeJS.",
+    link: "https://www.masonwallisatx.com/",
+    repo: "https://github.com/mwallis5110/My_Portfolio",
+  },
+];
+
+export const RelationalArray = [
+  {
+    cardImg: Employee,
+    cardTitle: "Employee Database",
+    cardDescription:
+      "Using Node.js, MYSQL, and InquirerJS, this program manages a company's employee database via a command-line interface.",
+    github: "https://github.com/mwallis5110/Employee_Tracker_HW12",
+  },
+];
+
+export const NonRelationalArray = [
+  {
+    cardImg: Fitness,
+    cardTitle: "FitnessTracker",
+    cardDescription:
+      "This program uses NoSQL and MongoDB to input and track your workout stats.",
+    link: "https://fitness-tracker-hw-15.herokuapp.com/",
+    github: "https://github.com/mwallis5110/Fitness_Tracker_HW_15",
+  },
+];
+
+export const GQLArray = [
+  {
+    cardImg: PortfolioGen,
+    cardTitle: "Portfolio Generator",
+    cardDescription:
+      "An application that utilizes GraphQL, React, and MongoDB to generate a detailed and shareable portfolio based on user inputs.",
+    link: "https://react-portfolio-generator-uta.herokuapp.com/",
+    repo: "https://github.com/mwallis5110/React_Portfolio_Generator",
+  },
+];
+
+export const JavaArray = [];
+
+export const MiscArray = [];
